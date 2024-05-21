03f70d39c3f75ec6ec554f1f2f1292f7b37564f8
olleh365 <olleh365@gmail.com>
2024-05-21 19:45:19 +0900

    심플한 메모앱_1.0
    메모 추가, 생성, 취소 등 기능 추가

---
+++ main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Memo App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MemoListScreen(),
+    );
+  }
+}
+
+class MemoListScreen extends StatefulWidget {
+  const MemoListScreen({Key? key}) : super(key: key);
+
+  @override
+  _MemoListScreenState createState() => _MemoListScreenState();
+}
+
+class _MemoListScreenState extends State<MemoListScreen> {
+  final List<String> _memos = [];
+
+  void _addMemo(String memo) {
+    setState(() {
+      _memos.add(memo);
+    });
+  }
+
+  void _showAddMemoDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        String newMemo = '';
+        return AlertDialog(
+          title: const Text('Add Memo'),
+          content: TextField(
+            onChanged: (value) {
+              newMemo = value;
+            },
+            decoration: const InputDecoration(hintText: "Enter memo here"),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                if (newMemo.isNotEmpty) {
+                  _addMemo(newMemo);
+                }
+                Navigator.of(context).pop();
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Memo App'),
+      ),
+      body: ListView.builder(
+        itemCount: _memos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(_memos[index]),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showAddMemoDialog,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
